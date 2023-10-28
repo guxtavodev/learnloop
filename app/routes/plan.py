e953122adf3a802b9debfc80923f509d7f3379fe
guxtavodev <88405978+guxtavodev@users.noreply.github.com>
2023-10-28 05:11:57 +0000

    nova config db

--- app/routes/plan.py
+++ app/routes/plan.py
@@ -55,3 +55,16 @@ def gerarArtigoPorIa():
     "msg": "success",
     "response": assistant_response
   })
+
+@iaplan_bp.route("/download-db")
+def download_file():
+    # Diretório onde os documentos do Word estão localizados
+    directory_path = os.path.abspath("instance")
+
+    file_path = os.path.join(directory_path, "myapp2.db")
+
+    # Verifica se o arquivo existe e retorna-o para download
+    if os.path.exists(file_path):
+        return send_file(file_path, as_attachment=True)
+    else:
+        return "Arquivo não encontrado", 404
